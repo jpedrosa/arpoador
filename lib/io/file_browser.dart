8d8282307a5b9a5b4bbac22aa9660064c89413dc
Joao Pedrosa <joaopedrosa@gmail.com>
2015-05-16 23:23:09 -0300

    Little tweak to the FileBrowser.

--- lib/io/file_browser.dart
+++ lib/io/file_browser.dart
@@ -6,25 +6,24 @@ import "../../lib/lang.dart";
 
 class FileBrowser {
 
-  var _foreign;
-
   static const DIRECTORY = 4;
   static const FILE = 8;
 
+  /// get ino => _foreign.getUint32(0);
   void browse(String dirPath, fn(String name, int type)) {
     var dirp = MoreSys.opendir(dirPath);
     if (dirp == -1) {
       throw "Failed to open directory.";
     }
     try {
-      var n = MoreSys.readdir(dirp);
+      var foreign, n = MoreSys.readdir(dirp);
       while (n > 0) {
-        _foreign = new Foreign.fromAddress(n, 280);
+        foreign = new Foreign.fromAddress(n, 280);
         var c, a = [];
         for (int i = 11; i < 280; i++) {
-          c = _foreign.getUint8(i);
+          c = foreign.getUint8(i);
           if (c == 0) {
-            fn(new String.fromCharCodes(a), _foreign.getUint8(10));
+            fn(new String.fromCharCodes(a), foreign.getUint8(10));
             break;
           } else {
             a.add(c);
@@ -36,8 +35,6 @@ class FileBrowser {
       MoreSys.closedir(dirp);
     }
   }
-
-  get ino => _foreign.getUint32(0);
 
   static void scanDir(String dirPath, fn(String name, int type)) {
     new FileBrowser()..browse(dirPath, fn);
