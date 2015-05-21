6baa5f93328ba7da8adf3ee2b83059b3735191cb
Joao Pedrosa <joaopedrosa@gmail.com>
2015-05-21 14:20:06 -0300

    Annotated the types of the IO APIs.

--- lib/io/io.dart
+++ lib/io/io.dart
@@ -7,39 +7,40 @@ import "stdout.dart";
 
 class IO {
 
-  static read(filePath) {
+  static String read(String filePath) {
     var s;
     File.open(filePath, 'r', (f) => s = f.read());
     return s;
   }
 
-  static readLines(filePath) {
+  static List<String> readLines(String filePath) {
     var a;
     File.open(filePath, 'r', (f) => a = f.readLines());
     return a;
   }
 
-  static readBytes(filePath) {
+  static List<int> readBytes(String filePath) {
     var a;
     File.open(filePath, 'r', (f) => a = f.readBytes());
     return a;
   }
 
-  static readWholeBuffer(filePath) {
+  static ByteBuffer readWholeBuffer(String filePath) {
     var b;
     File.open(filePath, 'r', (f) => b = f.readWholeBuffer());
     return b;
   }
 
-  static write(filePath, string) {
+  static void write(String filePath, String string) {
     File.open(filePath, 'w', (f) => f.write(string));
   }
 
-  static writeBytes(filePath, List<int> bytes) {
+  static void writeBytes(String filePath, List<int> bytes) {
     File.open(filePath, 'w', (f) => f.writeBytes(bytes));
   }
 
-  static void popenReadLines(command, fn(String string), [lineLength = 80]) {
+  static void popenReadLines(var command, fn(String string), 
+      [int lineLength = 80]) {
     PopenStream.readLines(command, fn, lineLength);
   }
 
