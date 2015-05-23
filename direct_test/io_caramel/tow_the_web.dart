ea2f3d5d53bf30b16932582c27284f4693bb1ef1
Joao Pedrosa <joaopedrosa@gmail.com>
2015-05-23 14:44:04 -0300

    Added a client socket test.

---
+++ direct_test/io_caramel/tow_the_web.dart
@@ -0,0 +1,17 @@
+import "dart:io";
+import "../../lib/fletch_helper.dart";
+import "../../lib/io/file.dart";
+import "../../lib/lang.dart";
+
+
+main() {
+  var socket = new Socket.connect("127.0.0.1", 8777),
+    command = "GET /that/one HTTP1/1\r\n\r\n",
+    list = FletchHelper.stringToUint8List(command);
+  socket.write(list.buffer);
+  var b = socket.readNext();
+  if (b != null) {
+    p(new String.fromCharCodes(b.asUint8List()));
+  }
+  p("ok");
+}
