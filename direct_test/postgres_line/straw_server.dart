a9acb0f09b9dc1900dcbd5be8db4d6e80ef4d41f
Joao Pedrosa <joaopedrosa@gmail.com>
2015-06-01 22:08:37 -0300

    Added packet throtling to the Straw Server test.

--- direct_test/postgres_line/straw_server.dart
+++ direct_test/postgres_line/straw_server.dart
@@ -2,7 +2,31 @@ import "dart:io" as DIO;
 import "dart:typed_data";
 import "../../lib/lang.dart";
 import "../../lib/io/io.dart";
+import "../../lib/sys/moresys.dart";
 
+
+final PACKET_SIZE = 8192;
+
+writePackets(socket, buffer) {
+  var len = buffer.lengthInBytes,
+    list = new Uint8List(PACKET_SIZE),
+    listBuffer = list.buffer,
+    listAddress = listBuffer.getForeign().value,
+    bufferAddress = buffer.getForeign().value,
+    offset = 0, size = PACKET_SIZE;
+  while (offset < len) {
+    if (offset + PACKET_SIZE >= len) {
+      size = len - offset;
+      list = new Uint8List(size);
+      listBuffer = list.buffer;
+      listAddress = listBuffer.getForeign().value;
+    }
+    MoreSys.memcpyMemToMem(listAddress, bufferAddress + offset, size);
+    socket.write(listBuffer);
+    DIO.sleep(10);
+    offset += size;
+  }
+}
 
 handleSocket(socket) {
   try {
@@ -10,7 +34,8 @@ handleSocket(socket) {
     if (b != null) {
       socket.write(IO.readWholeBuffer("start_up_bytes"));
       while ((b = socket.readNext()) != null) {
-        socket.write(IO.readWholeBuffer("query_response_bytes"));
+        //socket.write(IO.readWholeBuffer("query_response_bytes"));
+        writePackets(socket, IO.readWholeBuffer("query_response_bytes"));
         break;
       }
     }
